c7b6dcd5ea9fdd42623a8001944db51c25f614ad
Emiliano Alvarez <emilianoalvarez91@gmail.com>
2025-08-08 16:29:23 +0200

    Remove old SDK Implementation

--- Package.swift
+++ Package.swift
@@ -7,28 +7,18 @@ let package = Package(
     name: "AttendiSpeechService",
     defaultLocalization: "nl",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AttendiSpeechService",
             targets: ["AttendiSpeechService"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AttendiSpeechService",
-            dependencies: [],
+            path: "AttendiSpeechService/AttendiSpeechService",
             resources: [.process("Resources")]
-        ),
-        .testTarget(
-            name: "AttendiSpeechServiceTests",
-            dependencies: ["AttendiSpeechService"]),
+        )
     ]
 )
